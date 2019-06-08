0b74b9f84f36efe338ead24bf1ca06aeaca281f7
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2019-06-08 09:49:18 +0000

    changed files names to be appropriate

--- run_deep_planner.sh
+++ run_deep_planner.sh
@@ -2,4 +2,4 @@
 rosrun rviz_tools_py markers.py &
 rosrun deep_planner yaw_server.py &
 roslaunch odom_to_trajectory ekf_create_trajectory.launch &
-rosrun deep_planner sensors_to_map_final.py
+rosrun deep_planner sensors_to_map.py
