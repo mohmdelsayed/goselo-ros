45e71d0ec97cf0c9c26e65fee9b5fbf132f388db
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2019-06-13 09:11:58 +0000

    a typo removal

--- run_goselo_ecosystem.sh
+++ run_goselo_ecosystem.sh
@@ -2,5 +2,5 @@
 roslaunch odom_to_trajectory ekf_create_trajectory.launch &
 rosrun rviz_tools_py markers.py &
 rosrun deep_planner goselo_network.py &
-rosrun deep_planner laster_to_directions.py &
+rosrun deep_planner laser_to_directions.py &
 rosrun deep_planner sensors_to_map.py
