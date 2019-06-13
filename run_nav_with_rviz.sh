fa24980a763db0af454f15fc5f2fdb63487e2f97
Mohamed Elsayed <mohamed.elsayed.1995@gmail.com>
2019-06-13 09:07:08 +0000

    removed rosbot from repo

--- run_nav_with_rviz.sh
+++ run_nav_with_rviz.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 sudo killall gzserver
 sudo killall gzclient
 sudo killall rviz
 sudo killall roscore
 sudo killall rosmaster
 
-roslaunch rosbot_description rosbot_rviz_gmapping.launch
+roslaunch betabot_description betabot_rviz_gmapping.launch
